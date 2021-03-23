f7a75a9312648fab19ffa3574a50f2da592cfdf7
nadav sholev <nadavschwalb@mail.tau.ac.il>
2021-03-23 12:57:18 +0200

    coding and decoding OK, pending decode_to_bits

--- EX1/src/hamming.cpp
+++ EX1/src/hamming.cpp
@@ -10,21 +10,31 @@
 #include <fstream>
 #include <time.h>
 
-
-void file_to_bits(char* file_block,char* bits);
-void parity_solver(char* inbits, char* outbits);
+template<typename T>
+void decoded_to_bits(T* file_block,char* bits, int nbits);
+void encoded_to_bits(int* file_block,char* bits, int nbits);
+void parity_encoder(char* inbits, char* outbits);
+void parity_decoder(char* inbits, char* outbits);
 void reverseArray(char* arr, int arr_len);
 void print_arr(char* arr,int arr_len);
-int merge_hamming(char* outbits);
+int merge_hamming(char* outbits,int nbits);
 void hamming_encoder(char* bits,int* encoded);
+void hamming_decoder(char* bits,int* decoded);
+
 
 int main(int argc, char** argv){
+
+  //global variables
   int iResult;
   char readbuf [UNCODED_LEN];
   char bits[UNCODED_LEN*BYTE];
+  int recvbuf[UNCODED_LEN];
   int encoded[BYTE];
   char inbits[UNCODED_LEN];
   char outbits[ENCODED_LEN];
+  char encoded_bits[ENCODED_LEN*BYTE];
+
+
   FILE *fp = fopen("../Dtelem.jpg", "r" );
   fseek(fp,0,SEEK_END);
   printf("file length in bytes: %ld\n",ftell(fp));
@@ -32,6 +42,7 @@ int main(int argc, char** argv){
   if(iResult != 0){
     printf("seek error: %S\n",strerror(iResult));
   }
+
 
   int read_count;
   
@@ -50,25 +61,49 @@ int main(int argc, char** argv){
           fclose(fp);
           return 1;
       }
-      file_to_bits(readbuf,bits);
+      decoded_to_bits(readbuf,bits,UNCODED_LEN);
+      printf("uncoded bits: ");
+      print_arr(bits,UNCODED_LEN*BYTE);
       hamming_encoder(bits,encoded);
-
+      encoded_to_bits(encoded,encoded_bits,ENCODED_LEN);
+      printf("encoded bits: ");
+      print_arr(encoded_bits,ENCODED_LEN*BYTE);
+      hamming_decoder(encoded_bits,recvbuf);
+      decoded_to_bits(recvbuf,bits,UNCODED_LEN);
+      printf("decoded bits: ");
+      print_arr(bits,UNCODED_LEN*BYTE);
+      
 
   }
   return 0;
 }
 
-void file_to_bits(char* file_block,char* bits){
+
+//---------------------functions-------------------------
+
+template<typename T>
+void decoded_to_bits(T* file_block,char* bits, int nbits){
   int bit_index =0;
-  for(int i=0;i<UNCODED_LEN;i++){
+  for(int i=0;i<nbits;i++){
     for(int j=BYTE-1;j>=0;j--){ //get bits from byte
+      *(bits+bit_index) = ((T)file_block[i] &(1 << j)) >> j;
+      bit_index++;
+    }
+  }
+}
+
+void encoded_to_bits(int* file_block,char* bits, int nbits){
+  int bit_index =0;
+  for(int i=0;i<nbits;i++){
+    for(int j=2*BYTE-2;j>=0;j--){ //get bits from byte
       *(bits+bit_index) = (file_block[i] &(1 << j)) >> j;
       bit_index++;
     }
   }
 }
 
-void parity_solver(char* inbits, char* outbits){
+
+void parity_encoder(char* inbits, char* outbits){
   //parity bit 1: 2,4,6,8,10,12,14
   //parity bit 2: 2,5,6,9,10,13,14
   //parity bit 4: 4,5,6,11,12,13,14
@@ -92,27 +127,86 @@ void parity_solver(char* inbits, char* outbits){
   reverseArray(outbits,ENCODED_LEN);
 }
 
-int merge_hamming(char* outbits){
-  reverseArray(outbits,ENCODED_LEN);
+void parity_decoder(char* inbits, char* outbits){
+  int errnum =0;
+  char result =0;
+  reverseArray(inbits,ENCODED_LEN);
+  //check parities
+  result = inbits[2]^inbits[4]^inbits[6]^inbits[8]^inbits[10]^inbits[12]^inbits[14];
+  if(result!=inbits[0]){
+    errnum+=1;
+    printf("error detected: 0 bit\n");
+  }
+  result = inbits[2]^inbits[5]^inbits[6]^inbits[9]^inbits[10]^inbits[13]^inbits[14];
+  if(result!=inbits[1]){
+    errnum+=1;
+    printf("error detected: 1 bit\n");
+  }
+  result = inbits[4]^inbits[5]^inbits[6]^inbits[11]^inbits[12]^inbits[13]^inbits[14];
+  if(result!=inbits[3]){
+    errnum+=1;
+    printf("error detected: 3 bit\n");
+  }
+  result = inbits[8]^inbits[9]^inbits[10]^inbits[11]^inbits[12]^inbits[13]^inbits[14];
+  if(result!=inbits[7]){
+    errnum+=1;
+    printf("error detected: 7 bit\n");
+  }
+  printf("number of errors: %d\n",errnum);
+  //fill outbits with decoded message
+  outbits[0] = inbits[2]; outbits[1] = inbits[4]; outbits[2] = inbits[5]; 
+  outbits[3] = inbits[6]; outbits[4] = inbits[8]; outbits[5] = inbits[9];
+  outbits[6] = inbits[10]; outbits[7] = inbits[11]; outbits[8] = inbits[12];
+  outbits[9] = inbits[13]; outbits[10] = inbits[14];
+  reverseArray(outbits,UNCODED_LEN);
+}
+
+int merge_hamming(char* outbits,int nbits){
+  reverseArray(outbits,nbits);
   int result = 0;
-  for(int i=0;i<ENCODED_LEN;i++){
+  for(int i=0;i<nbits;i++){
     result += outbits[i]<<i;
   }
   return result;
 }
 
+
+
 void hamming_encoder(char* bits,int* encoded){
+  printf("encoder in: ");
+  print_arr(bits,UNCODED_LEN*BYTE);
   char inbits[UNCODED_LEN];
   char outbits[ENCODED_LEN];
+  printf("encoder out: ");
   for(int i=0;i<BYTE;i++){
     //create sub array of 11 bits
     for(int j=0;j<UNCODED_LEN;j++){
       inbits[j] = bits[i*UNCODED_LEN +j];
     }
     //encode inbits into outbits array
-    parity_solver(inbits,outbits);
+    parity_encoder(inbits,outbits);
+    print_arr(outbits,ENCODED_LEN);
     //merge encoded bits into int array
-    encoded[i] = merge_hamming(outbits);
+    encoded[i] = merge_hamming(outbits,ENCODED_LEN);
+  }
+}
+
+void hamming_decoder(char* bits,int* decoded){
+  printf("decoder in: ");
+  print_arr(bits,ENCODED_LEN*BYTE);
+  char inbits[ENCODED_LEN];
+  char outbits[UNCODED_LEN];
+  printf("decoder out: ");
+  for(int i=0;i<BYTE;i++){
+    //create sub array of 15 bits
+    for(int j=0;j<ENCODED_LEN;j++){
+      inbits[j] = bits[i*ENCODED_LEN +j];
+    }
+    //encode inbits into outbits array
+    parity_decoder(inbits,outbits);
+    print_arr(outbits,UNCODED_LEN);
+    //merge encoded bits into int array
+    decoded[i] = merge_hamming(outbits,UNCODED_LEN);
   }
 }
 
