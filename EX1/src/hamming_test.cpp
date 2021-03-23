fb29b1e3f5ceba7ea98383354b9daafdfa843743
nadav sholev <nadavschwalb@mail.tau.ac.il>
2021-03-23 15:33:26 +0200

    hamming works creating lib

--- EX1/src/hamming_test.cpp
+++ EX1/src/hamming_test.cpp
@@ -1,5 +1,27 @@
+#define UNCODED_LEN 11
+#define ENCODED_LEN 15
+#define BYTE 8
 
-#include "hamming.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <time.h>
+
+
+template<typename T>
+void get_bits(T* block,char* bits, int nbits, int nelem);
+void parity_encoder(char* inbits, char* outbits);
+int parity_decoder(char* inbits, char* outbits);
+void reverseArray(char* arr, int arr_len);
+template<typename T>
+void print_arr(T* arr,int arr_len);
+int merge_hamming(char* outbits,int nbits);
+void hamming_encoder(char* bits,int* encoded);
+void hamming_decoder(char* bits,int* decoded);
+
 
 int main(int argc, char** argv){
 
